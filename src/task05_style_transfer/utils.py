3ce81ff378714cb2f16ac7c9842383c3905bf199
Rakib 007 <152210130+rak9b@users.noreply.github.com>
2024-12-29 22:03:07 +0600

    Create utils.py

---
+++ src/task05_style_transfer/utils.py
@@ -0,0 +1,12 @@
+def load_image(image_path):
+    """
+    Load an image from the file system.
+    """
+    pass
+
+
+def save_image(image, output_path):
+    """
+    Save an image to the file system.
+    """
+    pass

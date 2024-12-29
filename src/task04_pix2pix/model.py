a0e8dc7f2c0f858f5945e8f90218a80df2528c7c
Rakib 007 <152210130+rak9b@users.noreply.github.com>
2024-12-29 22:00:53 +0600

    Create model.py

---
+++ src/task04_pix2pix/model.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+
+
+class Pix2PixGenerator(nn.Module):
+    # Define the generator network here
+    pass
+
+
+class Pix2PixDiscriminator(nn.Module):
+    # Define the discriminator network here
+    pass
